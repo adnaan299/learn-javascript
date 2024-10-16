464c8eef0049f55c8954873037fe031e37d9acc9
Md Adnaan Nasir <111058596+adnaan299@users.noreply.github.com>
2024-10-16 06:42:39 +0000

    changes 4

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3);  // we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // code readability should be high
+
+let name = "adnaan"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value // it can be assigned
+// undefined => value is not defined
+// symbol => (Unique)Symbols are often used to add unique property keys to an object 
+
+// object 
+
+console.log(typeof null); // O/P (object)
+console.log(typeof null); // undefined
+
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,59 @@
+// let score = 'adnaan'
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// result of conversions // line 6
+// "33" => 33
+// "33eww" => NaN(Not a Number)(but it is also a type of number)
+// true => 1; false => 0
+
+
+// let isLoggedIn = 1;
+// let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "adnaan" => true
+
+// let someNumber = 33
+// let stringNumber = String(someNumber);
+// console.log(typeof stringNumber);
+
+
+// ********************* OPERATIONS ********************
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2) // for 2 power 2
+// console.log(2/3)
+// console.log(2%3)
+
+// let str1 = "hello"
+// let str2 = " adnaan"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+2); // => 12
+// console.log(1+"2"); // => 12
+// console.log("1" + 2 + 2); // => 122
+// console.log(1 + 2 + "2"); // => 32
+
+// ****** Not preferred ****
+// console.log(+true);  
+// console.log(+"");
+// **************************
+
+  
+
