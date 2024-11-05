44ad52d225a8aa04d887b410cd0074358347a6c0
Md Adnaan Nasir <111058596+adnaan299@users.noreply.github.com>
2024-11-05 19:38:29 +0530

    Create one.js

---
+++ 05_iterations/one.js
@@ -0,0 +1,21 @@
+// for
+
+for(let i = 0; i < 10; i++){
+    console.log (i);
+    if(i == 5){
+        console.log("5 is best number")
+    }
+}
+
+let myArray = ["flash", "batman", "superman"]
+
+for(let i = 0; i < myArray.length; i++){
+    console.log(myArray[i]);
+}
+
+// keywords
+// break => take you out of the loop and break the flow;
+// continue =>bas ek baar conditon chalega
+//terminates execution of the statements in the current 
+// iteration of the current or labeled loop, and continues execution
+//  of the loop with the next iteration.
