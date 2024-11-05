d56ab4eab37c37aba66d337277ee85eed89390d4
Md Adnaan Nasir <111058596+adnaan299@users.noreply.github.com>
2024-11-05 19:37:31 +0530

    Create switch.js

---
+++ 04_controlFLow/switch.js
@@ -0,0 +1,28 @@
+// switch(key){
+//     case value:
+
+//         break;
+
+//     default:
+
+//         break;
+// }
+
+const month = 3
+
+switch(month){
+    case 1:
+        console.log("jan");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("mar");
+        break;
+    
+
+}
+// if break not present it will not match
+// another case after one match and execute
+// all the code after that EXCEPT DEFAULT
