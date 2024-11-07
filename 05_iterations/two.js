9b1489f913b5fc29dbc548b519a45a7a17bb3531
Md Adnaan Nasir <111058596+adnaan299@users.noreply.github.com>
2024-11-07 15:38:20 +0530

    Create two.js

---
+++ 05_iterations/two.js
@@ -0,0 +1,14 @@
+let i = 0;
+while(i < 10){
+    console.log(`value of index is ${i}`)
+    i = i + 2;
+}
+// do while loop
+// do{    
+// }while(conditon);
+
+let score = 0;
+do{
+    console.log(`score is ${score}`)
+    score++
+}while(score <= 10);
